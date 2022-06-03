0010b222a9faca6cc6e7bd9d1981a70e1c557c91
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-06-03 09:32:44 -0300

    layout-login-cadastro-ok

--- src/components/1.TelaLogin/TelaLogin.js
+++ src/components/1.TelaLogin/TelaLogin.js
@@ -1,9 +1,106 @@
-
+import React from 'react';
+import { useContext } from "react";
+import UserContext from "../../UserContext";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import styled from 'styled-components';
+import logo from "../../assets/logo_inicial.png";
 
 
 export default function TelaLogin () {
 
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    //const { setToken } = useContext(UserContext);
+
+    // const navigate = useNavigate();
+
+    // function fazerLogin (event) {
+    //     event.preventDefault(); 
+
+    //     const login = {
+    //         email: email,
+    //         password: senha
+    //     }
+        
+    //     const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login', login);
+    //     promise.then(res => {
+    //         setToken(res.data.token);
+    //         console.log(res.data.token);
+    //         navigate('/hoje')}); 
+    //         promise.catch(err => {
+    //             console.log(err);
+    //             alert("Dados incorretos! Preencha os campos novamente.")});
+
+
     return (
-        <div>TelaLogin</div>
+        <Container>
+            <img src={logo} width="320px" height="62px"/>
+            <form /*onSubmit={fazerLogin}*/>
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="  E-mail"></input>
+                <input type="password" value={senha} onChange={e => setSenha(e.target.value)} placeholder="  Senha"></input>
+                <button type="submit">ENTRAR</button>
+            </form>
+            <div>
+                <Link style={{textDecoration: 'none'}} to="/sign-up">
+                    <h2>Não possui uma conta? Cadastre-se</h2>
+                </Link>
+            </div>
+        </Container>
     );
 }
+
+const Container = styled.div`
+
+    width: 100%;
+    height: 100%;
+    padding-top: 12px;
+    padding-left: 38px;
+    padding-right: 38px;
+    padding-bottom: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        margin-top: 122px;
+        margin-bottom: 100px;
+    }
+
+    input {
+        width: 100%;
+        height: 52px;
+        border: none;
+        border-radius: 8px;
+        background-color: var(--cor-branco);
+        margin-bottom: 16px;
+        font-size: 14px;
+        color: var(--cor-cinza-escuro);
+        ::placeholder {
+            font-size: 14px;
+            font-weight: 400;
+            color: var(--cor-cinza-claro);
+        }
+    }
+
+    button {
+        width: 100%;
+        height: 52px;
+        background-color: var(--cor-rosa-driven);
+        font-size: 14px;
+        font-weight: 700;
+        color: var(--cor-branco);
+        border: none;
+        border-radius: 8px;
+        margin-bottom: 24px;
+    }
+
+    h2 {
+        color: var(--cor-branco);
+    }
+    
+
+`
+
--- src/components/2.TelaCadastro/TelaCadastro.js
+++ src/components/2.TelaCadastro/TelaCadastro.js
@@ -1,7 +1,106 @@
+import React from 'react';
+import { useContext } from "react";
+import UserContext from "../../UserContext";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import styled from 'styled-components';
+import logo from "../../assets/logo_inicial.png";
 
 export default function TelaCadastro () {
 
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+
+    // const navigate = useNavigate();
+
+
+    // function fazerCadastro (event) {
+    //     event.preventDefault();
+        
+    //     const signup = {
+    //         email: email,
+    //         name: nome,
+    //         image: foto,
+    //         password: senha
+    //     }
+        
+    //     const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up', signup);
+    //     promise.then(res => {
+    //         console.log(res.data);
+    //         navigate('/')}); 
+    //     promise.catch(err => {
+    //         console.log(err);
+    //         alert("Preencha os dados corretamente, por favor.");
+    //     });
+
+    //}
+
     return (
-        <div>TelaCadastro</div>
+        <Container>
+        <form /*onSubmit={fazerLogin}*/>
+        <input type="text" value={nome} onChange={e => setNome(e.target.value)} placeholder="  Nome"></input>
+            <input type="text" value={cpf} onChange={e => setCpf(e.target.value)} placeholder="  CPF"></input>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="  E-mail"></input>
+            <input type="password" value={senha} onChange={e => setSenha(e.target.value)} placeholder="  Senha"></input>
+            <button type="submit">CADASTRAR</button>
+        </form>
+        <div>
+            <Link style={{textDecoration: 'none'}} to="/">
+                <h2>Já possui uma conta? Entre</h2>
+            </Link>
+        </div>
+    </Container>
     );
 }
+
+const Container = styled.div`
+
+    width: 100%;
+    height: 100%;
+    padding-top: 147px;
+    padding-left: 38px;
+    padding-right: 38px;
+    padding-bottom: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    input {
+        width: 100%;
+        height: 52px;
+        border: none;
+        border-radius: 8px;
+        background-color: var(--cor-branco);
+        margin-bottom: 16px;
+        font-size: 14px;
+        color: var(--cor-cinza-escuro);
+        ::placeholder {
+            font-size: 14px;
+            font-weight: 400;
+            color: var(--cor-cinza-claro);
+        }
+    }
+
+    button {
+        width: 100%;
+        height: 52px;
+        background-color: var(--cor-rosa-driven);
+        font-size: 14px;
+        font-weight: 700;
+        color: var(--cor-branco);
+        border: none;
+        border-radius: 8px;
+        margin-bottom: 24px;
+    }
+
+    h2 {
+        color: var(--cor-branco);
+    }
+    
+
+`
+
